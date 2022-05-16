9fe06dd7669acf9937562b326c7b0020bd7697fd
swapneethpunna <swapneethpunna@msitprogram.net>
2022-05-17 00:00:44 +0530

    Fsf

--- CMS_Project/CMS_Project/asgi.py
+++ CMS_Project/CMS_Project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms.settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CMS_Project.settings.settings')
 
 application = get_asgi_application()
--- CMS_Project/CMS_Project/wsgi.py
+++ CMS_Project/CMS_Project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cms.settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CMS_Project.settings.settings')
 
 application = get_wsgi_application()
